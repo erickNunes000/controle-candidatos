5b00582f8ef08078d1f33f8cac5d426fd46f9af2
Erick Nunes <erickFernandes000@gmail.com>
2025-02-09 23:34:14 -0300

    add method selecionar candidados

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,41 @@
 package candidatura;
 
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900);
-        analisarCandidato(2200);
-        analisarCandidato(2000);
+        // analisarCandidato(1900);
+        // analisarCandidato(2200);
+        // analisarCandidato(2000);
+        selecaoCandidatos();
+
+    }
+    static void selecaoCandidatos(){
+        String[] candidatos = {"felipe","marcia","julia","paulo","augusto","monica","fabricio","mirela","daniela","jorge"};
+        ArrayList<String> candidatosSelecionados = new ArrayList<String>();
+        int selecionados=0;
+        for(int i=0;i<candidatos.length;i++){
+            if(selecionados==5){
+                break;
+            }
+            double salPretendido = gerarSalPretendido();
+            System.out.println("nome: "+candidatos[i]+" salario pretendido: "+salPretendido);
+            if(salPretendido<=2000){
+                candidatosSelecionados.add(candidatos[i]);
+                selecionados++;
+            }
+        }
+        for (String string : candidatosSelecionados) {
+            System.out.println(string);
+        }
+
+    }
+    static double gerarSalPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
     static void analisarCandidato(double salPretendido){
+
         double salBase = 2000.0;
         if(salPretendido>salBase){
             System.out.println("Aguardando resultado dos demais candidatos");
@@ -16,4 +45,6 @@ public class ProcessoSeletivo {
             System.out.println("Ligar para o candidato");
         }
     }
+
+    
 }
